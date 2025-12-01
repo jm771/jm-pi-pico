eb8896f72827d7a19a0d2283bf232e4826d59f4f
JackMills <JackMills@NotRealEmail.com>
2025-12-01 11:30:26 -0600

    initial commit

---
+++ hello_world.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main()
+{
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    stdio_init_all();
+
+    while (true)
+    {
+        gpio_put(PICO_DEFAULT_LED_PIN, true);
+        sleep_ms(100);
+        gpio_put(PICO_DEFAULT_LED_PIN, false);
+        sleep_ms(100);
+        printf("Hello, world!\n");
+    }
+
+    return 0;
+}

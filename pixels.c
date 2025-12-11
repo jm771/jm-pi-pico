555fc54c73404adf2e01e11be533ee645df06918
JackMills <JackMills@NotRealEmail.com>
2025-12-11 17:32:32 -0500

    bug fixes

--- pixels.c
+++ pixels.c
@@ -10,16 +10,16 @@ void pixels_produce_output(unsigned int frame, uint32_t *buffer)
 
     memset(buffer, 0, sizeof(uint32_t) * N_LEDS);
 
-    for (size_t i = 0; i < N_ROWS / 3; i++)
+    for (size_t i = 0; i < (ROW_WIDTH / 3); i++)
     {
-        for (size_t j = 0; i < 3; j++)
+        for (size_t j = 0; j < 3; j++)
         {
             uint32_t color = rand();
-            for (size_t i2 = 0; i < 3; i++)
+            for (size_t i2 = 0; i2 < 3; i2++)
             {
-                for (size_t j2 = 0; j < 3; j++)
+                for (size_t j2 = 0; j2 < 3; j2++)
                 {
-                    write_pixel(buffer, i * 3 + i2, j * 3 + j2, color);
+                    write_pixel(buffer, j * 3 + j2, i * 3 + i2, color);
                 }
             }
         }

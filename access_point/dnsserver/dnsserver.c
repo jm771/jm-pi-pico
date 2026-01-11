39b5dfb8623fe5d7bf66f8f6cbfa4fe0ab60c823
JackMills <JackMills@NotRealEmail.com>
2026-01-11 14:20:44 -0500

    compiling - about to test

--- access_point/dnsserver/dnsserver.c
+++ access_point/dnsserver/dnsserver.c
@@ -19,7 +19,8 @@
 #define DEBUG_printf(...)
 #define ERROR_printf printf
 
-typedef struct dns_header_t_ {
+typedef struct dns_header_t_
+{
     uint16_t id;
     uint16_t flags;
     uint16_t question_count;
@@ -30,27 +31,33 @@ typedef struct dns_header_t_ {
 
 #define MAX_DNS_MSG_SIZE 300
 
-static int dns_socket_new_dgram(struct udp_pcb **udp, void *cb_data, udp_recv_fn cb_udp_recv) {
+static int dns_socket_new_dgram(struct udp_pcb **udp, void *cb_data, udp_recv_fn cb_udp_recv)
+{
     *udp = udp_new();
-    if (*udp == NULL) {
+    if (*udp == NULL)
+    {
         return -ENOMEM;
     }
     udp_recv(*udp, cb_udp_recv, (void *)cb_data);
     return ERR_OK;
 }
 
-static void dns_socket_free(struct udp_pcb **udp) {
-    if (*udp != NULL) {
+static void dns_socket_free(struct udp_pcb **udp)
+{
+    if (*udp != NULL)
+    {
         udp_remove(*udp);
         *udp = NULL;
     }
 }
 
-static int dns_socket_bind(struct udp_pcb **udp, uint32_t ip, uint16_t port) {
+static int dns_socket_bind(struct udp_pcb **udp, uint32_t ip, uint16_t port)
+{
     ip_addr_t addr;
     IP4_ADDR(&addr, ip >> 24 & 0xff, ip >> 16 & 0xff, ip >> 8 & 0xff, ip & 0xff);
     err_t err = udp_bind(*udp, &addr, port);
-    if (err != ERR_OK) {
+    if (err != ERR_OK)
+    {
         ERROR_printf("dns failed to bind to port %u: %d", port, err);
         assert(false);
     }
@@ -58,13 +65,18 @@ static int dns_socket_bind(struct udp_pcb **udp, uint32_t ip, uint16_t port) {
 }
 
 #if DUMP_DATA
-static void dump_bytes(const uint8_t *bptr, uint32_t len) {
+static void dump_bytes(const uint8_t *bptr, uint32_t len)
+{
     unsigned int i = 0;
 
-    for (i = 0; i < len;) {
-        if ((i & 0x0f) == 0) {
+    for (i = 0; i < len;)
+    {
+        if ((i & 0x0f) == 0)
+        {
             printf("\n");
-        } else if ((i & 0x07) == 0) {
+        }
+        else if ((i & 0x07) == 0)
+        {
             printf(" ");
         }
         printf("%02x ", bptr[i++]);
@@ -73,13 +85,16 @@ static void dump_bytes(const uint8_t *bptr, uint32_t len) {
 }
 #endif
 
-static int dns_socket_sendto(struct udp_pcb **udp, const void *buf, size_t len, const ip_addr_t *dest, uint16_t port) {
-    if (len > 0xffff) {
+static int dns_socket_sendto(struct udp_pcb **udp, const void *buf, size_t len, const ip_addr_t *dest, uint16_t port)
+{
+    if (len > 0xffff)
+    {
         len = 0xffff;
     }
 
     struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, len, PBUF_RAM);
-    if (p == NULL) {
+    if (p == NULL)
+    {
         ERROR_printf("DNS: Failed to send message out of memory\n");
         return -ENOMEM;
     }
@@ -89,7 +104,8 @@ static int dns_socket_sendto(struct udp_pcb **udp, const void *buf, size_t len, 
 
     pbuf_free(p);
 
-    if (err != ERR_OK) {
+    if (err != ERR_OK)
+    {
         ERROR_printf("DNS: Failed to send message %d\n", err);
         return err;
     }
@@ -100,15 +116,18 @@ static int dns_socket_sendto(struct udp_pcb **udp, const void *buf, size_t len, 
     return len;
 }
 
-static void dns_server_process(void *arg, struct udp_pcb *upcb, struct pbuf *p, const ip_addr_t *src_addr, u16_t src_port) {
+static void dns_server_process(void *arg, struct udp_pcb *upcb, struct pbuf *p, const ip_addr_t *src_addr, u16_t src_port)
+{
+    (void)upcb;
     dns_server_t *d = arg;
     DEBUG_printf("dns_server_process %u\n", p->tot_len);
 
     uint8_t dns_msg[MAX_DNS_MSG_SIZE];
-    dns_header_t *dns_hdr = (dns_header_t*)dns_msg;
+    dns_header_t *dns_hdr = (dns_header_t *)dns_msg;
 
     size_t msg_len = pbuf_copy_partial(p, dns_msg, sizeof(dns_msg), 0);
-    if (msg_len < sizeof(dns_header_t)) {
+    if (msg_len < sizeof(dns_header_t))
+    {
         goto ignore_request;
     }
 
@@ -129,19 +148,22 @@ static void dns_server_process(void *arg, struct udp_pcb *upcb, struct pbuf *p, 
     // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 
     // Check QR indicates a query
-    if (((flags >> 15) & 0x1) != 0) {
+    if (((flags >> 15) & 0x1) != 0)
+    {
         DEBUG_printf("Ignoring non-query\n");
         goto ignore_request;
     }
 
     // Check for standard query
-    if (((flags >> 11) & 0xf) != 0) {
+    if (((flags >> 11) & 0xf) != 0)
+    {
         DEBUG_printf("Ignoring non-standard query\n");
         goto ignore_request;
     }
 
     // Check question count
-    if (question_count < 1) {
+    if (question_count < 1)
+    {
         DEBUG_printf("Invalid question count\n");
         goto ignore_request;
     }
@@ -151,16 +173,22 @@ static void dns_server_process(void *arg, struct udp_pcb *upcb, struct pbuf *p, 
     const uint8_t *question_ptr_start = dns_msg + sizeof(dns_header_t);
     const uint8_t *question_ptr_end = dns_msg + msg_len;
     const uint8_t *question_ptr = question_ptr_start;
-    while(question_ptr < question_ptr_end) {
-        if (*question_ptr == 0) {
+    while (question_ptr < question_ptr_end)
+    {
+        if (*question_ptr == 0)
+        {
             question_ptr++;
             break;
-        } else {
-            if (question_ptr > question_ptr_start) {
+        }
+        else
+        {
+            if (question_ptr > question_ptr_start)
+            {
                 DEBUG_printf(".");
             }
             int label_len = *question_ptr++;
-            if (label_len > 63) {
+            if (label_len > 63)
+            {
                 DEBUG_printf("Invalid label\n");
                 goto ignore_request;
             }
@@ -171,7 +199,8 @@ static void dns_server_process(void *arg, struct udp_pcb *upcb, struct pbuf *p, 
     DEBUG_printf("\n");
 
     // Check question length
-    if (question_ptr - question_ptr_start > 255) {
+    if (question_ptr - question_ptr_start > 255)
+    {
         DEBUG_printf("Invalid question length\n");
         goto ignore_request;
     }
@@ -181,9 +210,9 @@ static void dns_server_process(void *arg, struct udp_pcb *upcb, struct pbuf *p, 
 
     // Generate answer
     uint8_t *answer_ptr = dns_msg + (question_ptr - dns_msg);
-    *answer_ptr++ = 0xc0; // pointer
+    *answer_ptr++ = 0xc0;                         // pointer
     *answer_ptr++ = question_ptr_start - dns_msg; // pointer to question
-    
+
     *answer_ptr++ = 0;
     *answer_ptr++ = 1; // host address
 
@@ -196,14 +225,14 @@ static void dns_server_process(void *arg, struct udp_pcb *upcb, struct pbuf *p, 
     *answer_ptr++ = 60; // ttl 60s
 
     *answer_ptr++ = 0;
-    *answer_ptr++ = 4; // length
+    *answer_ptr++ = 4;                  // length
     memcpy(answer_ptr, &d->ip.addr, 4); // use our address
     answer_ptr += 4;
 
     dns_hdr->flags = lwip_htons(
-                0x1 << 15 | // QR = response
-                0x1 << 10 | // AA = authoritative
-                0x1 << 7);   // RA = authenticated
+        0x1 << 15 | // QR = response
+        0x1 << 10 | // AA = authoritative
+        0x1 << 7);  // RA = authenticated
     dns_hdr->question_count = lwip_htons(1);
     dns_hdr->answer_record_count = lwip_htons(1);
     dns_hdr->authority_record_count = 0;
@@ -217,12 +246,15 @@ ignore_request:
     pbuf_free(p);
 }
 
-void dns_server_init(dns_server_t *d, ip_addr_t *ip) {
-    if (dns_socket_new_dgram(&d->udp, d, dns_server_process) != ERR_OK) {
+void dns_server_init(dns_server_t *d, ip_addr_t *ip)
+{
+    if (dns_socket_new_dgram(&d->udp, d, dns_server_process) != ERR_OK)
+    {
         DEBUG_printf("dns server failed to start\n");
         return;
     }
-    if (dns_socket_bind(&d->udp, 0, PORT_DNS_SERVER) != ERR_OK) {
+    if (dns_socket_bind(&d->udp, 0, PORT_DNS_SERVER) != ERR_OK)
+    {
         DEBUG_printf("dns server failed to bind\n");
         return;
     }
@@ -230,6 +262,7 @@ void dns_server_init(dns_server_t *d, ip_addr_t *ip) {
     DEBUG_printf("dns server listening on port %d\n", PORT_DNS_SERVER);
 }
 
-void dns_server_deinit(dns_server_t *d) {
+void dns_server_deinit(dns_server_t *d)
+{
     dns_socket_free(&d->udp);
 }
--- main.c
+++ main.c
@@ -16,6 +16,7 @@
 #include "dma_helpers.h"
 // #include "pico/status_led.h"
 #include "pico/cyw43_arch.h"
+#include "picow_access_point.h"
 
 #define WS2812_FREQ 800000
 #define OTHER_LED_PIN 0
@@ -75,7 +76,9 @@ void main_init()
 
 void set_onboard_led(bool on)
 {
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
+    (void)on;
+    // Turning off for now as the demo also uses the LED
+    // cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
     // gpio_put(WL_GPIO0, on);
 #ifdef PICO_DEFAULT_LED_PIN
     gpio_put(PICO_DEFAULT_LED_PIN, on);
@@ -84,6 +87,61 @@ void set_onboard_led(bool on)
 #else
 #error Need to define some sort on on board LED
 #endif
+}
+
+static TCP_SERVER_T server_state;
+
+void access_point_init()
+{
+
+    TCP_SERVER_T *state = &server_state;
+
+    if (cyw43_arch_init())
+    {
+        DEBUG_printf("failed to initialise\n");
+        return;
+    }
+
+    const char *ap_name = "picow_test";
+    const char *password = "password";
+
+    cyw43_arch_enable_ap_mode(ap_name, password, CYW43_AUTH_WPA2_AES_PSK);
+
+    ip4_addr_t mask;
+    state->gw.addr = PP_HTONL(CYW43_DEFAULT_IP_AP_ADDRESS);
+    mask.addr = PP_HTONL(CYW43_DEFAULT_IP_MASK);
+
+    // Start the dhcp server
+    static dhcp_server_t dhcp_server;
+    dhcp_server_init(&dhcp_server, &state->gw, &mask);
+
+    // Start the dns server
+    static dns_server_t dns_server;
+    dns_server_init(&dns_server, &state->gw);
+
+    if (!tcp_server_open(state, ap_name))
+    {
+        DEBUG_printf("failed to open server\n");
+        // return 1;
+    }
+
+    // state->complete = false;
+    // while (!state->complete)
+    // {
+    //     // if you are using pico_cyw43_arch_poll, then you must poll periodically from your
+    //     // main loop (not from a timer interrupt) to check for Wi-Fi driver or lwIP work that needs to be done.
+
+    //     // TODO - could so this later
+    //     // you can poll as often as you like, however if you have nothing else to do you can
+    //     // choose to sleep until either a specified time, or cyw43_arch_poll() has work to do:
+    //     // cyw43_arch_wait_for_work_until(make_timeout_time_ms(1000));
+    // }
+    // tcp_server_close(state);
+    // dns_server_deinit(&dns_server);
+    // dhcp_server_deinit(&dhcp_server);
+    // cyw43_arch_deinit();
+    // printf("Test complete\n");
+    // return 0;
 }
 
 int main()
@@ -104,6 +162,7 @@ int main()
     while (true)
     {
         tud_task();
+        cyw43_arch_poll();
         if (letsReset)
         {
             blank_buffer(MainLedBuffer);

52893baed1f78d9328f5377b4696170018120f77
JackMills <JackMills@NotRealEmail.com>
2026-01-12 12:37:17 -0500

    still working

---
+++ access_point/picow_access_point.h
@@ -0,0 +1,35 @@
+#ifndef PICOW_ACCESS_POINT_H
+#define PICOW_ACCESS_POINT_H
+
+#include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+
+#include "lwip/pbuf.h"
+#include "lwip/tcp.h"
+
+#include "dhcpserver.h"
+#include "dnsserver.h"
+
+#define DEBUG_printf printf
+
+typedef struct TCP_SERVER_T_
+{
+    struct tcp_pcb *server_pcb;
+    ip_addr_t gw;
+} TCP_SERVER_T;
+
+typedef struct TCP_CONNECT_STATE_T_
+{
+    struct tcp_pcb *pcb;
+    int sent_len;
+    char headers[128];
+    char result[256];
+    int header_len;
+    int result_len;
+    ip_addr_t *gw;
+} TCP_CONNECT_STATE_T;
+
+bool tcp_server_open(TCP_SERVER_T *state, const char *ap_name);
+void tcp_server_close(TCP_SERVER_T *state);
+
+#endif

dd5bc4d01170ffb036b18f547332119c3b037392
Gwen Eidsmoe <gwen.eidsmoe@gmail.com>
2026-02-02 14:36:45 -0500

    multiple cars

--- light_programs/frogger.c
+++ light_programs/frogger.c
@@ -17,15 +17,15 @@ typedef struct
     int32_t x;
     int32_t y;
     int32_t resetTimer;
+    int32_t moveOnFrame;
 } car_pos_t;
 
 static bool hasLost = false;
 
 static frog_pos_t FrogPos;
-static car_pos_t Car1Pos;
-static car_pos_t Car2Pos;
-static car_pos_t Car3Pos;
 static bool hasWon;
+
+static car_pos_t car_positions[3];
 
 // void WrapPos(frog_pos_t *pos)
 // {
@@ -36,9 +36,14 @@ void frogger_init()
 {
     FrogPos.x = 0;
     FrogPos.y = 0;
-    Car1Pos.x = 2;
-    Car1Pos.y = N_FULL_ROWS - 1;
-    Car1Pos.resetTimer = 0;
+    for (int i = 0; i < 3; i++) {
+        car_positions[i].x = i*2 + 2;
+        car_positions[i].y = N_FULL_ROWS - 1;
+        car_positions[i].resetTimer = 0;
+    }
+    car_positions[0].moveOnFrame = 10;
+    car_positions[1].moveOnFrame = 5;
+    car_positions[2].moveOnFrame = 15; 
     hasLost = false;
     hasWon = false;
 }
@@ -51,20 +56,21 @@ void set_hat_full_color(uint32_t* buffer, uint32_t color)
             write_pixel(buffer, i, j, color);
 }
 
-void car_logic(car_pos_t *car, frog_pos_t* frog, uint32_t frame, uint32_t *buffer)
+// returns true if a car has hit the frog
+bool car_logic(car_pos_t *car, frog_pos_t* frog, uint32_t frame, uint32_t *buffer)
 {
     if (car->x == frog->x && car->y == frog->y && car->resetTimer == 0) {
         hasLost = true;
-        return;
+        return true;
     }
     
-    if (car->resetTimer >= 0)
+    if (car->resetTimer > 0)
     {
         car->resetTimer--;
     }
     else
     {
-        if (frame % 10 == 0)
+        if (frame % car->moveOnFrame == 0)
         {
             car->y--;
         }
@@ -81,6 +87,8 @@ void car_logic(car_pos_t *car, frog_pos_t* frog, uint32_t frame, uint32_t *buffe
     }
 
     printf("%li %li\n", car->y, car->resetTimer);
+
+    return false;
 }
 
 // This is the "render" call currently called every 20ms in main (20ms > 6ms so should be fine) (nextFrameTime = currTime + 20 * 1000;)
@@ -99,20 +107,21 @@ void frogger_produce_output(uint32_t frame, uint32_t *buffer)
     }
     else
     {
-        car_logic(&Car1Pos, &FrogPos, frame, buffer);
-
-        if (hasLost) {
-            set_hat_full_color(buffer, 0xFF0000);
-        } else {
-            // This API lets you "write" on a 21x10 grid - and for higher rows interpolates to find the nearest actual LED
-            // So our current frog controls are argubally bad (on higher rows left / right won't actually cause movement)
-            write_pixel(buffer, FrogPos.x, FrogPos.y, 0x00FF00);
-
-            // victory zone
-            for (int i = 0; i < N_FULL_ROWS; i++)
-            {
-                write_pixel(buffer, FULL_ROW_LEN - 1, i, 0x00FF00);
+        for (int i = 0; i < 3; i++) {
+            if (car_logic(&car_positions[i], &FrogPos, frame, buffer)) {
+                set_hat_full_color(buffer, 0xFF0000);
+                return;
             }
+        }
+
+        // This API lets you "write" on a 21x10 grid - and for higher rows interpolates to find the nearest actual LED
+        // So our current frog controls are argubally bad (on higher rows left / right won't actually cause movement)
+        write_pixel(buffer, FrogPos.x, FrogPos.y, 0x00FF00);
+
+        // victory zone
+        for (int i = 0; i < N_FULL_ROWS; i++)
+        {
+            write_pixel(buffer, FULL_ROW_LEN - 1, i, 0x00FF00);
         }
     }
 }

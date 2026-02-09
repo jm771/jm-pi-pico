d05fcdd5a2f7d99632870b23b22b5ca8540352a6
JackMills <JackMills@NotRealEmail.com>
2026-02-09 17:09:35 -0500

    working is

--- server/frogger_page.c
+++ server/frogger_page.c
@@ -2,7 +2,25 @@
 #include "frogger_page.h"
 #include "stdio.h"
 
-#define FROGGER_CONTENT "<html><body>Frogger! <button id=\"demo\">Button</button></body><script>document.getElementById(\"demo\").onclick = function() {myFunction()};function myFunction() { const request = new Request(\"/frogger.html/\", { method: \"POST\", body: 'd', }); fetch(request).then((response) => { if (response.status !== 200) { throw new Error(\"Something went wrong on API server!\"); } return response; }).catch((error) => { console.error(error); });};</script></html>"
+#define FROGGER_CONTENT                                                                                                      \
+    "<html><body>Frogger!<table>"                                                                                            \
+    "<tr><td/><td>"                                                                                                          \
+    "<button id=\"up\">Up</button>"                                                                                          \
+    "</td><td/></tr>"                                                                                                        \
+    "<tr><td>"                                                                                                               \
+    "<button id=\"left\">Left</button>"                                                                                      \
+    "</td><td/><td>"                                                                                                         \
+    "<button id=\"right\">Right</button>"                                                                                    \
+    "</td></tr>"                                                                                                             \
+    "<tr><td/><td>"                                                                                                          \
+    "<button id=\"down\">Down</button>"                                                                                      \
+    "</td><td/></tr>"                                                                                                        \
+    "</table></body><script>"                                                                                                \
+    "document.getElementById(\"left\").onclick = function() {fetch(\"/frogger.html/\", { method: \"POST\", body: 'a', })};"  \
+    "document.getElementById(\"right\").onclick = function() {fetch(\"/frogger.html/\", { method: \"POST\", body: 'd', })};" \
+    "document.getElementById(\"up\").onclick = function() {fetch(\"/frogger.html/\", { method: \"POST\", body: 'w', })};"    \
+    "document.getElementById(\"down\").onclick = function() {fetch(\"/frogger.html/\", { method: \"POST\", body: 's', })};"  \
+    "</script></html>"
 
 int serve_frogger_content(const char *params, char *result, size_t max_result_len)
 {
